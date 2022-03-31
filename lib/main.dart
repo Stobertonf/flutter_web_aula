5059e7c3da66a6e01475271a2616535f2d1b3c7c
Stoberton Francisco <49157502+Stobertonf@users.noreply.github.com>
2022-03-31 19:57:02 -0300

    feat:(device_preview): remove package and fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
+import 'orientacao.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_aula/loja_virtual.dart';
 import 'package:flutter_web_aula/regras.layout.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter_web_aula/tamanho_textos.dart';
 import 'package:flutter_web_aula/responsividade_row_col.dart';
 import 'package:flutter_web_aula/responsividade_media_query.dart';
-import 'orientacao.dart';
 
 void main() {
   runApp(
-    DevicePreview(
-      builder: (context) {
-        return MaterialApp(
-          title: "Flutter Web",
-          debugShowCheckedModeBanner: false,
-          builder: DevicePreview.appBuilder,
-          locale: DevicePreview.locale(context),
-        );
-      },
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // home: ReponsividadeMediaQuery(),
+      // home: ReponsividadeRowCol(),
+      // home: ResponsividadeWrap(),
+      // home: Orientacao(),
+      //home: RegrasLayout(),
+      home: LojaVirtual(),
+      //home: TamanhoTextos(),
     ),
   );
 }
